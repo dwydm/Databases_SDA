c139be2049e79a8fce88fe690efb4da4b8cc2736
dwydm <dwydmuch@gmail.com>
2022-09-04 16:16:57 +0200

    JDBC basic queries

--- src/main/java/jdbc/introduction/JDBCIntroduction.java
+++ src/main/java/jdbc/introduction/JDBCIntroduction.java
@@ -2,6 +2,7 @@ package jdbc.introduction;
 
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -22,13 +23,43 @@ public class JDBCIntroduction {
         String newDatabase = "CREATE DATABASE IF NOT EXISTS jdbc_example;";
         executeAnyUpdate(newDatabase);
         String dropDatabase = "DROP DATABASE jdbc_example;";
-        executeAnyUpdate(dropDatabase);
+        //executeAnyUpdate(dropDatabase);
+        String createTable =
+                "CREATE TABLE IF NOT EXISTS jdbc_example.Users (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY," +
+                "first_name VARCHAR(16) NOT NULL," +
+                "last_name VARCHAR(24) NOT NULL," +
+                "age INT NOT NULL," +
+                "language ENUM('PL','EN','FR','DE','ES'));";
+        executeAnyUpdate(createTable);
+
+        String recordsForDB =
+                "INSERT INTO jdbc_example.Users (first_name, last_name, age, language)" +
+                "VALUES ('Bob','Wood',22,'EN'), ('Anna','Colt',26,'PL'), ('Rob','Bold',33,'EN');";
+        //executeAnyUpdate(recordsForDB);
+
+        String displayTableContent = "SELECT * FROM jdbc_example.Users;";
+        executeAndDisplayQuery(displayTableContent);
 
 
         //close connection
         Configuration.closeConnection();
     }
 
+    public static void executeAndDisplayQuery(String statement) {
+        ResultSet rs;
+        try {
+            rs = stmt.executeQuery(statement);
+            while (rs.next()) {
+                System.out.println(rs.getString("first_name") + " " +
+                        rs.getString("last_name") + ", age: " +
+                        rs.getInt("age") + ", language: " +
+                        rs.getString("language"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void executeAnyUpdate(String statement) {
         try {
             stmt.executeUpdate(statement);
@@ -44,7 +75,5 @@ public class JDBCIntroduction {
             throw new RuntimeException(e);
         }
     }
-
-
 
 }
